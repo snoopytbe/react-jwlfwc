3df1a4bdb11f762fe4af0c12c957a85c41260900
snoopytbe <matthieu.andre.77@gmail.com>
2021-03-04 17:29:46 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
-import { theme, useStyles } from "./styles/styles";
-import Calendrier from "./components/Calendrier/Calendrier";
+//import { ThemeProvider } from "@material-ui/core/styles";
+//import { Typography } from "@material-ui/core";
+//import { theme, useStyles } from "./styles/styles";
+//import Calendrier from "./components/Calendrier/Calendrier";
 import Occupation from "./components/Occupation/Occupation";
 import TableauSynthese from "./components/Synthese/TableauSynthese";
+//import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { useRoutes } from "hookrouter";
+
+const routes = {
+  "/TableauSynthese": () => <TableauSynthese />,
+  "/Occupation": () => <Occupation />
+};
 
 export default function App() {
-  const classes = useStyles();
-  return (
+  //const classes = useStyles();
+  const routeResult = useRoutes(routes);
+  /*return (
     <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <Typography variant="h4">Calendrier</Typography>
-
-        <br />
-
-        <Occupation />
-        <br />
-      </div>
+      <BrowserRouter>
+        <div className={classes.root}>
+          <Typography variant="h4">
+            <Link to="/TableauSynthese">Synthese</Link>
+          </Typography>
+          <Typography variant="h4">
+            <Link to="/Occupation">Occupation</Link>
+          </Typography>
+          <br />
+        </div>
+        <div>
+          <Switch>
+            <Route path="/TableauSynthese" children={<TableauSynthese />} />
+            <Route path="/Occupation" component={Occupation} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+      
     </ThemeProvider>
-  );
+
+  );*/
+  return (routeResult || <NotFoundPage />)
 }
